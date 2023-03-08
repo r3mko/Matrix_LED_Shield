6f28ae9b4bd3936fe02e7775fbf948a00d167b94
Remko Kleinjan <r.a.kleinjan@gmail.com>
2023-03-08 21:50:11 +0100

    Code refactor and cleanup

---
+++ src/Matrix_LED.cpp
@@ -0,0 +1,71 @@
+#include "Matrix_LED.h"
+
+MLED::MLED(uint8_t mled_intensity, uint8_t mled_mosi, uint8_t mled_sclk) {
+	_mosi = mled_mosi;
+	_sclk = mled_sclk;
+
+	if (mled_intensity > 7) {
+		_intensity = 7;
+	} else {
+		_intensity = mled_intensity;
+	}
+
+	pinMode(_mosi, OUTPUT);
+	pinMode(_sclk, OUTPUT);
+	
+	digitalWrite(_mosi, HIGH);
+	digitalWrite(_sclk, HIGH);
+}
+
+void MLED::display() {
+	for (uint8_t i = 0; i < 8; i++) {
+		sendData(i, disBuffer[i]);
+
+		digitalWrite(_mosi, LOW);
+  	digitalWrite(_sclk, LOW);
+  	digitalWrite(_sclk, HIGH);
+  	digitalWrite(_mosi, HIGH);
+	}
+
+	sendCommand(0x88 | _intensity);
+}
+
+void MLED::clear() {
+	for (uint8_t i = 0; i < 8; i++) {
+		disBuffer[i] = 0x00;
+	}
+}
+
+void MLED::dot(uint8_t x, uint8_t y, bool draw) {
+	x &= 0x07;
+	y &= 0x07;
+
+	if (draw) {
+		disBuffer[y] |= (1 << x);
+	} else {
+		disBuffer[y] &= ~(1 << x);
+	}
+}
+
+void MLED::sendCommand(byte cmd) {
+  digitalWrite(_mosi, LOW);
+  send(cmd);
+  digitalWrite(_mosi, HIGH);
+}
+
+void MLED::sendData(byte address, byte data) {
+  sendCommand(0x44);
+  digitalWrite(_mosi, LOW);
+  send(0xC0 | address);
+  send(data);
+  digitalWrite(_mosi, HIGH);
+}
+
+void MLED::send(byte data) {
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(_sclk, LOW);
+    digitalWrite(_mosi, data & 1 ? HIGH : LOW);
+    data >>= 1;
+    digitalWrite(_sclk, HIGH);
+  }
+}
---
+++ src/Matrix_LED.h
@@ -0,0 +1,27 @@
+
+#ifndef Matrix_LED_H
+#define Matrix_LED_H
+
+#include <Arduino.h>
+
+#define MLED_MOSI 13
+#define MLED_SCLK 14
+
+class MLED {
+public:
+	MLED(uint8_t mled_intensity, uint8_t mled_mosi = MLED_MOSI, uint8_t mled_sclk = MLED_SCLK);
+	void display();
+	void clear();
+	void dot(uint8_t x, uint8_t y, bool draw = 1);
+	volatile uint8_t disBuffer[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+private:
+	void sendCommand(byte led);
+	void sendData(byte add, byte data);
+	void send(byte data);
+	uint8_t _mosi;
+	uint8_t _sclk;
+	uint8_t _intensity;
+};
+
+#endif
